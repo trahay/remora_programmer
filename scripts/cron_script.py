1ff73cad6dc48397256e23403aabffabd6afff11
François Trahay <francois.trahay@telecom-sudparis.eu>
2018-10-01 23:39:18 +0200

    communication avec Remora

--- scripts/cron_script.py
+++ scripts/cron_script.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient;
 import pprint;
-from bson.objectid import ObjectId
-import datetime
+from bson.objectid import ObjectId;
+import datetime;
+import requests;
 
 weekday=datetime.datetime.today().weekday();
 client = MongoClient('mongodb://localhost/');
@@ -25,15 +26,53 @@ def get_hour_index():
     hour= datetime.datetime.now().hour;
     minute= datetime.datetime.now().minute;
     return minute/15+4*hour;    
-    
+
+def get_current_mode(url):
+    response=requests.get(url)
+    return response.json()
+
+def set_mode(url, name, mode):
+    response = requests.put(url, json={name:mode});
+    return response
+
+def mode(int_mode):
+    if int_mode == 0:
+        return "A"
+    if int_mode == 1:
+        return "H"
+    if int_mode == 2:
+        return "E"
+    if int_mode == 3:
+        return "C"
+    return ""
+
 for z in zones.find():
     try:
         zone_name=z['name'];
-        zone_url=z['url'];
+        zone_url=z['url'];      # full url (eg. http://192.168.0.10/fp3)
+        zone_url_name=zone_url.split("/")[-1]; # last token (eg. fp3)
+        zone_base_url="/".join(zone_url.split("/")[:-1]); # base url (eg. http://192.168.0.10 )
+        zone_id=zone_url_name[-1];                        # id (eg. 3)
+        
         zone_program=z['program']; 
         program=get_program_semaine(zone_program);
         program_journee=get_program_journee(program['program'][weekday]);
-        cur_program=program_journee['program'][get_hour_index()];
-        print("name:"+zone_name+"  url:"+zone_url+" program:"+program_journee['name']+"  cur_program:"+str(cur_program));
+
+        cur_program=program_journee['program'][get_hour_index()]; # program to apply
+
+        current_mode=get_current_mode(zone_url); # program currently in use
+
+        m = mode(cur_program);
+
+        req_url=zone_base_url+"?setfp="+zone_id+mode(cur_program);
+
+        print("Setting zone "+zone_name+" to mode "+m+ "\tfull url: "+req_url);
+
+        response=get_current_mode(req_url);
+        if response['response'] == 0:
+            print("\tOK")
+        else:
+            print("\tKO")
     except:
         print("An error occured !");
+
