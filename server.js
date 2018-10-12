70afec2428311f11f54b9305af16e13b073bd0ec
François Trahay <francois.trahay@telecom-sudparis.eu>
2018-10-12 23:30:05 +0200

    debut de portage vers sqlite

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ var app = express();
 var util = require('util');
 var path = require('path');
 var fs = require('fs');
-var db = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectID;
 
-db.connect("mongodb://localhost/", function(error, database) {
-    if (error) return funcCallback(error);
-    db=database.db("chauffage");
-
-    console.log("Connecté à la base de données 'chauffage'");
-});;
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./db_chauffage.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
 
 var morgan = require('morgan');
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'logs/access.log'), {flags: 'a'});
@@ -73,27 +72,27 @@ function supprime_prog_sem(req, res) {
 }
 
 function edit_prog_sem(req, res) {
-
-    db.collection("programme_semaine").findOne(
-	{ "_id":  ObjectId(req.query.id) },
-	(err, selected_prog) => {
+    let sql=`select * from programme_semaine where id=?`;
+    db.all(sql, req.query.id, (err, selected_prog) => {
 	    if(err) throw err;
-	    if(selected_prog == null) {
+	    if(selected_prog.length == 0) {
 		console.log("cannot find "+req.query.id);
 		return afficher_program_sem(req, res);
 	    } else {
 		if(err) throw err;
-		req.selected_prog = selected_prog;
+		req.selected_prog = selected_prog[0];
 		return afficher_program_sem(req, res);
 	    }
-	});
-
+    });
 }
 
 function afficher_program_sem(req, res) {
     var selected_prog=req.selected_prog;
-    db.collection("programme_semaine").find().toArray((err, result_prog) => {
-	db.collection("programme_journee").find().toArray((err, progs_jour) => {
+    let sql=`select * from programme_semaine`;
+    db.all(sql, [], (err, result_prog) => {
+	if(err) throw err;
+	let sql=`select * from programme_journee`;
+	db.all(sql, [], (err, progs_jour) => {
 	    if(err) throw err;
 	    console.log(selected_prog);
 	    res.render('afficher_program_sem.ejs',
@@ -111,13 +110,16 @@ function afficher_logs(req, res) {
 app.get('/', (req, res) => {
     var d=new Date().toISOString().
 	replace(/T/, ' ').      // replace T with a space
-	replace(/\..+/, '')     // delete the dot and everything after
+	replace(/\..+/, '');     // delete the dot and everything after
 
-    db.collection("zones").find().toArray((err, result) =>{
+    let sql_zone=`select * from zones`;
+    let sql_prog_sem=`select * from programme_semaine`;
+    let sql_prog_jour=`select * from programme_journee`;
+    db.all(sql_zone, [], (err, result) => {
 	if(err) console.log(err);
-	db.collection("programme_semaine").find().toArray((err, programs) => {
+	db.all(sql_prog_sem, [], (err, programs) => {
 	    if(err) console.log(err);
-	    db.collection("programme_journee").find().toArray((err, progs_jour) => {
+	    db.all(sql_prog_jour, [], (err, progs_jour) => {
 		if(err) console.log(err);
 		res.render('index.ejs', {today:d,
 					 zones:result,
@@ -132,81 +134,75 @@ app.get('/', (req, res) => {
 app.post('/ajouter_zone', (req, res) => {
     if(req.body.id.length == 0) {
 	// new zone
-	db.collection("zones").save(
-	    {
-		"name":req.body.name,
-		"url":req.body.url,
-		"program":req.body.program,
-	    }, (err, results) => {
-		if (err) return console.log(err);
-		console.log("add zone: "+req.body.name+ " - url="+req.body.url+" - program="+req.body.program);
-		console.log('saved zone to database');
-		res.redirect('/afficher_zones');
-	    });
+	let sql='insert into zones (name, url, program) values (?, ?, ?)';
+	db.run(sql, req.body.name, req.body.url, req.body.program, (err) => {
+	    if (err) return console.log(err);
+	    console.log("add zone: "+req.body.name+ " - url="+req.body.url+" - program="+req.body.program);
+	    console.log('saved zone to database');
+	    res.redirect('/afficher_zones');
+	});
     } else {
 	// edit an existing zone
 
-	db.collection("zones").update(
-	    {"_id": ObjectId(req.body.id)},
-	    {
-		"name":req.body.name,
-		"url": req.body.url,
-		"program":req.body.program,
-	    })
-	    .then((success) => {
-		console.log("edit zone: "+req.body.name+ " - url="+req.body.url+" - program="+req.body.program);
-		console.log('saved zone to database');
-		res.redirect('/afficher_zones');
-	    })
-	    .catch((error) => {
+	let sql='update zones set name=?, url=?, program=? where id=?';
+	db.run(sql, req.body.name, req.body.url, req.body.program, req.body.id, (err) => {
+	    if(err) {
 		console.log(error);
-	    });
+		return;
+	    }
+	    console.log("edit zone: "+req.body.name+ " - url="+req.body.url+" - program="+req.body.program);
+	    console.log('saved zone to database');
+	    res.redirect('/afficher_zones');
+	});
     }
 });
 
 app.get('/edit_zones', (req, res) => {
 
-    db.collection("zones").findOne(
-	{ "_id":  ObjectId(req.query.id) },
-	(err, selected_zone) => {
+    let sql='select * from zones where id=?';
+    db.all(sql, req.query.id, (err, selected_zone) => {
 	    if(err) throw err;
-	    if(selected_zone == null) {
+	    if(selected_zone.length == 0) {
 		console.log("cannot find "+req.query.id);
 	    } else {
 		if(err) throw err;
-		console.log(selected_zone);
-		db.collection("programme_semaine").find().toArray((err, programs) => {
+		console.log(selected_zone[0]);
+		let sql='select * from programme_semaine';
+		db.all(sql, [], (err, programs) => {
 		    if(err) throw err;
 
-		    db.collection("zones").find().toArray((err, result_zones) =>{
+		    let sql='select * from zones';
+		    db.all(sql, [], (err, result_zones) => {
+			if(err) throw err;
 			res.render('afficher_zones.ejs',
 				   {zones: result_zones,
-				    selected_zone:selected_zone,
+				    selected_zone:selected_zone[0],
 				    selected_prog:"",
 				    programs:programs});
 		    });
 		});
 	    }
-	});
+    });
 });
 
 app.get('/supprime_zone', (req, res) => {
-    db.collection("zones").remove(
-	{ "_id":  ObjectId(req.query.id) },
-	(err, document) => {
-	    if(err) throw err;
-	    console.log("Zone supprimée: "+req.query.id);
 
-	    res.redirect('/afficher_zones');
-	});
+    let sql = 'DELETE FROM zones where id=?';
+    db.run(sql, req.query.id, (err)=> {
+	if(err) throw err;
+	console.log("Zone supprimée: "+req.query.id);
 
+	res.redirect('/afficher_zones');
+    });
 });
 
 app.get('/afficher_zones', (req, res) => {
-
-    db.collection("zones").find().toArray((err, result_zones) =>{
+    let sql='select * from zones';
+    db.all(sql, [], (err, result_zones) =>{
 	if(err) throw err;
-	db.collection("programme_semaine").find().toArray((err, programs) => {
+
+	let sql='select * from programme_semaine';
+	db.all(sql, [], (err, programs) =>{
 	    if(err) throw err;
 	    res.render('afficher_zones.ejs',
 		       {zones: result_zones,
@@ -218,7 +214,8 @@ app.get('/afficher_zones', (req, res) => {
 });
 
 app.get('/afficher_programs', (req, res) => {
-    db.collection("programme_journee").find().toArray((err, result_prog) => {
+    let sql='select * from programme_journee';
+    db.all(sql, [], (err, result_prog) => {
 	res.render('afficher_programs.ejs',
 		   {progs: result_prog,
 		    selected_prog:"",
@@ -276,81 +273,73 @@ function createProg(start, stop, mode, prog) {
     console.log("->from "+start+" to "+stop);
     for(i=start; i<stop; i++)
 	prog[i]=mode;
-    return prog;
+    return prog.join('');
 }
 
 app.post('/ajouter_prog', (req, res) => {
     if(req.body.id.length == 0) {
 	// new program
 	prog=createProg(req.body.heure_debut, req.body.heure_fin, req.body.mode);
-	db.collection("programme_journee").save(
-	    {
-		"program":prog,
-		"name":req.body.name
+	let sql='insert into programme_journee(program, name) values(?, ?)';
+	db.run(sql, prog, req.body.name, (err) => {
+	    if (err) {
+		console.log("Error while running "+sql+", "+prog+", "+req.body.name);
+		console.log(err);
+		return;
 	    }
-	    , (err, results) => {
-	    if (err) return console.log(err);
 	    console.log('saved program '+req.body.name+' to database');
 	    res.redirect('/afficher_programs');
-	    });
+	});
     } else {
-	db.collection("programme_journee").findOne(
-	    { "_id":  ObjectId(req.body.id) },
-	    (err, selected_prog) => {
-		if(err) throw err;
-
-		prog=createProg(req.body.heure_debut, req.body.heure_fin, req.body.mode, selected_prog.program);
-		db.collection("programme_journee").update(
-		    {"_id": ObjectId(req.body.id)},
-		    {
-			"program":prog,
-			"name":req.body.name
-		    })
-		    .then((success) => {
-			console.log('edit program '+req.body.name+' to database');
-			console.log('saved program to database');
-			res.redirect('/edit_prog?id='+req.body.id);
-		    })
-		    .catch((error) => {
-			console.log(error);
-		    });
-
+	let sql='select * from programme_journee where id=?';
+	db.all(sql, req.body.id, (err, selected_prog) => {
+	    if(err) throw err;
+	    
+	    prog=createProg(req.body.heure_debut, req.body.heure_fin, req.body.mode, selected_prog[0].program);
+	    let sql='update programme_journee set program=?, name=? where id=?';
+	    db.run(sql, prog, req.body.name, req.body.id, (err) => {
+		if(err) {
+		    console.log(err);
+		} else {
+		    console.log('edit program '+req.body.name+' to database');
+		    console.log('saved program to database');
+		}
+		res.redirect('/edit_prog?id='+req.body.id);
 	    });
-
+	    
+	});
     }
 });
 
 app.get('/supprime_prog', (req, res) => {
-    db.collection("programme_journee").remove(
-	{ "_id":  ObjectId(req.query.id) },
-	(err, document) => {
-	    if(err) throw err;
-	    console.log("Programme supprimée: "+req.query.id);
-
-	    res.redirect('/afficher_programs');
-	});
+    let sql='delete from programme_journee where id=?';
+    db.run(sql, req.query.id, (err)=> {
+	if(err) throw err;
+	console.log("Programme supprimée: "+req.query.id);
+	res.redirect('/afficher_programs');
+    });
 });
 
 app.get('/edit_prog', (req, res) => {
-
-    db.collection("programme_journee").findOne(
-	{ "_id":  ObjectId(req.query.id) },
-	(err, selected_prog) => {
+    let sql='select * from programme_journee where id=?';
+    db.all(sql, req, req.query.id, (err, selected_prog) => {
+	if(err) throw err;
+	if(selected_prog.length == 0) {
+	    console.log("cannot find "+req.query.id);
+	} else {
 	    if(err) throw err;
-	    if(selected_prog == null) {
-		console.log("cannot find "+req.query.id);
-	    } else {
-		if(err) throw err;
-		db.collection("programme_journee").find().toArray((err, result_prog) =>{
-
-		    res.render('afficher_programs.ejs',
-			       {progs: result_prog,
-				selected_prog:selected_prog,
-				util:util});
-		});
-	    }
-	});
-
+	    let sql='select * from programme_journee';
+	    db.all(sql, [], (err, result_prog) =>{
+		if(err) {
+		    console.log(err.message);
+		}
+		res.render('afficher_programs.ejs',
+			   {progs: result_prog,
+			    selected_prog:selected_prog[0],
+			    util:util});
+	    });
+	}
+    });
 });
 
 
@@ -368,57 +357,65 @@ app.post('/ajouter_prog_sem', (req, res) => {
 
     if(req.body.id.length == 0) {
 	// new program
-	db.collection("programme_semaine").save(
-	    {
-		"name":req.body.prog_name,
-		"program":program,
-	    }, (err, results) => {
-		if (err) return console.log(err);
-		console.log('saved program_semaine '+req.body.prog_name+' to database');
-		res.redirect('/program_semaine');
-	    });
+	let sql='insert into programme_semaine (name, prog_0, prog_1, prog_2, prog_3, prog_4, prog_5, prog_6) values(?, ?, ?, ?, ?, ?, ?, ?)';
+	db.run(sql, req.body.prog_name,
+	       req.body.prog_0,
+	       req.body.prog_1,
+	       req.body.prog_2,
+	       req.body.prog_3,
+	       req.body.prog_4,
+	       req.body.prog_5,
+	       req.body.prog_6, (err) => {
+		   if (err) return console.log(err);
+		   console.log('saved program_semaine '+req.body.prog_name+' to database');
+		   res.redirect('/program_semaine');
+	       });
 	
     } else {
 	// update
-
-	db.collection("programme_semaine").update(
-	    {"_id": ObjectId(req.body.id)},
-	    {
-		"name":name,
-		"program":program
-	    })
-	    .then((success) => {
-		console.log("edit programme_semaine: "+name+": "+req.body);
-		console.log('saved zone to database');
+	let sql='update programme_semaine set name=?, prog_0=?, prog_1=?, prog_2=?, prog_3=?, prog_4=?, prog_5=?, prog_6=? where id=?';
+	db.run(sql, name,
+	       req.body.prog_0,
+	       req.body.prog_1,
+	       req.body.prog_2,
+	       req.body.prog_3,
+	       req.body.prog_4,
+	       req.body.prog_5,
+	       req.body.prog_6,
+	       req.body.id, (err) => {
+		   if(err) {
+		       console.log(err.message);
+		   } else {
+		       console.log("edit programme_semaine: "+name+": "+req.body);
+		       console.log('saved zone to database');
+		   }
 		res.redirect('/program_semaine');
-	    })
-	    .catch((error) => {
-		console.log(error);
-	    });
-
+	       });
     }
 });
 
 app.get('/supprime_prog_sem', (req, res) => {
-    db.collection("programme_semaine").remove(
-	{ "_id":  ObjectId(req.query.id) },
-	(err, document) => {
-	    if(err) throw err;
-	    console.log("Programme semaine supprimé: "+req.query.id);
-	    return afficher_program_sem(req, res);
-	});
+    let sql='delete from programme_semaine where id=?';
+    db.run(sql, req.query.id, (err)=> {
+	if(err) throw err;
+	console.log("Programme semaine supprimé: "+req.query.id);
+	return afficher_program_sem(req, res);
+    });
 });
 
 app.get('/edit_prog_sem', edit_prog_sem);
 
 app.get('/program_semaine', (req, res) => {
-    db.collection("programme_semaine").find().toArray((err, result_prog) => {
-	db.collection("programme_journee").find().toArray((err, progs_jour) => {
+    let sql='select * from programme_semaine';
+    db.all(sql, [], (err, result_prog) => {
+	if(err) throw err;
+	let sql='select * from programme_journee';
+	db.all(sql, [], (err, progs_jour) => {
 	    if(err) throw err;
 	    res.render('afficher_program_sem.ejs',
-		   {progs: result_prog,
-		    selected_prog:"",
-		    progs_jour:progs_jour});
+		       {progs: result_prog,
+			selected_prog:"",
+			progs_jour:progs_jour});
 	});
     });
 });
