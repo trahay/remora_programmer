409b1ac4ce146bffa95457c9ddd1d119c1fb3cd9
François Trahay <francois.trahay@telecom-sudparis.eu>
2023-09-24 14:50:52 +0200

    fix cron log file

--- install_cron.sh
+++ install_cron.sh
@@ -6,5 +6,5 @@ prefix=$(dirname $full_path)
 cron_log="/var/log/remora_programmer/cron.log"
 
 
-script_path="python $prefix/scripts/cron_script.py $prefix/db_chauffage.db >> $prefix/logs/cron.log"
+script_path="python $prefix/scripts/cron_script.py $prefix/db_chauffage.db >> $cron_log"
 (crontab -l 2>/dev/null; echo "*/5 * * * * $script_path ") | crontab -
